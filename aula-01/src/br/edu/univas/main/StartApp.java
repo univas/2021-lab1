35446478aa3eb4967582c19f52c586721e8f74ef
Rodrigo Faria <rodrigo.faria@nexadigital.com.br>
2021-03-13 14:45:38 -0300

    Add aula-01

---
+++ aula-01/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,26 @@
+package br.edu.univas.main;
+
+public class StartApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+		
+		int age = 15;
+		long a = 10;
+		short b = 11;
+		byte c = 12;
+		double d = 25.6;
+		float e = 99.9f;
+		char f = 'R';
+		boolean g = true;
+		 
+		System.out.println(age);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+	}
+}

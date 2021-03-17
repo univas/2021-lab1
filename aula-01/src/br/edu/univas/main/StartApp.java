c50d5a49da4e9fab26670b383696358dfd6e335e
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-03-17 11:52:46 -0300

    Creating String variables

--- aula-01/src/br/edu/univas/main/StartApp.java
+++ aula-01/src/br/edu/univas/main/StartApp.java
@@ -1,10 +1,13 @@
 package br.edu.univas.main;
 
 public class StartApp {
-	
+	/*
+	  comentario de 
+	  bloco
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!");
-		
+		//variaveis primitivas!!!
 		int age = 15;
 		long a = 10;
 		short b = 11;
@@ -13,6 +16,7 @@ public class StartApp {
 		float e = 99.9f;
 		char f = 'R';
 		boolean g = true;
+		boolean isRainning = false;
 		 
 		System.out.println(age);
 		System.out.println(a);
@@ -22,5 +26,24 @@ public class StartApp {
 		System.out.println(e);
 		System.out.println(f);
 		System.out.println(g);
+		
+		f = 'X';
+		System.out.println(a + "-" + b + "-" + c + 
+				"-" + d + "-" + e + "-" + f + "-" + g);
+		
+		f = 'Y';
+		f = 'A';
+		String nomeAluno = "João 1234";
+		System.out.println(nomeAluno);
+		
+		//criar 2 variaveis:
+		//1 com primeiro nome
+		//outra com seu sobrenome
+		//imprimir seu nome completo em uma instrução só
+		String nome = "Rodrigo";
+		String sobreNome = "Faria";
+		//concatenação
+		System.out.println(nome + " " + sobreNome);
+		System.out.println("Rodrigo Faria");
 	}
 }

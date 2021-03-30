4fe4ef18e039025eef70b54f80e70e36c1684e2f
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-03-29 22:07:41 -0300

    Resolve exercises 2 and 3

---
+++ exercicios/src/br/edu/univas/exercicios/Questao2.java
@@ -0,0 +1,35 @@
+package br.edu.univas.exercicios;
+
+public class Questao2 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		
+		if (x == 0) {
+			System.out.println("Zero");
+		} else if (x == 1) {
+			System.out.println("Um");
+		} else if (x == 2) {
+			System.out.println("Dois");
+		} else if (x == 3) {
+			System.out.println("Três");
+		} else if (x == 4) {
+			System.out.println("Quatro");
+		} else if (x == 5) {
+			System.out.println("Cinco");
+		} else if (x == 6) {
+			System.out.println("Seis");
+		} else if (x == 7) {
+			System.out.println("Sete");
+		} else if (x == 8) {
+			System.out.println("Oito");
+		} else if (x == 9) {
+			System.out.println("Nove");
+		} else if (x == 10) {
+			System.out.println("Dez");
+		} else {
+			System.out.println("Por favor, digite um número entre 0 e 10!");
+		}
+	}
+	
+}
---
+++ exercicios/src/br/edu/univas/exercicios/Questao3.java
@@ -0,0 +1,23 @@
+package br.edu.univas.exercicios;
+
+public class Questao3 {
+
+	public static void main(String[] args) {
+		int idade = 15;
+		
+		if (idade <= 7) {
+			System.out.println("Infantil A");
+		} else if (idade <= 10) {
+			System.out.println("Infantil B");
+		} else if (idade <= 13) {
+			System.out.println("Juvenil A");
+		} else if (idade <= 17) {
+			System.out.println("Juvenil B");
+		} else if (idade <= 60) {
+			System.out.println("Adulto");
+		} else {
+			System.out.println("Sênior");
+		}
+		
+	}
+}

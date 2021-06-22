17941fb601c28f14bc2275b9792b62b85572905e
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-06-22 20:05:01 -0300

    Add questao35

---
+++ exercicios/src/br/edu/univas/exercicios/Questao35.java
@@ -0,0 +1,29 @@
+package br.edu.univas.exercicios;
+
+import java.util.Scanner;
+
+public class Questao35 {
+
+	public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		
+		int[] values = new int[50];
+		
+		for (int i = 0; i < 50; i++) {
+			System.out.println("Por favor, digite um número:");
+			values[i] = leitura.nextInt();
+		}
+		
+		int counterNegativeNumbers = 0;
+		for (int i = 0; i < 50; i++) {
+			if (values[i] < 0) {
+				values[i] = 0;
+				counterNegativeNumbers++;
+			}
+		}
+		
+		System.out.println("Foram substituidos " + counterNegativeNumbers + " números negativos por zero.");
+		
+		leitura.close();
+	}
+}
